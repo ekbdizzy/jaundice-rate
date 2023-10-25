a47b4a9e66e195ba7fbc79cf4255c2032ddc08f4
Aleksey Koshkin <ekbdizzy@yandex.ru>
2023-10-25 09:43:24 +0700

    Add exceptions handlers to the process_article

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import asyncio
+from enum import Enum
 import os
 
 import aiofiles
 import aiohttp
 import anyio
+from async_timeout import timeout
 import pymorphy2
 
 from adapters.inosmi_ru import sanitize
+from adapters.exceptions import ArticleNotFound
 from text_tools import split_by_words, calculate_jaundice_rate
 
+TIMEOUT = 5
+
 TEST_ARTICLES = [
+    "https://inosmi.ru/not/exist.html",
+    "https://lenta.ru/brief/2021/08/26/afg_terror/",
     "https://inosmi.ru/20231021/neandertaltsy-266231832.html",
     "https://inosmi.ru/20231014/solntse-266120679.html",
     "https://inosmi.ru/20231009/obezyana-266007731.html",
     "https://inosmi.ru/20231009/lunatizm-265977298.html",
     "https://inosmi.ru/20231024/seks-266274274.html",
 ]
+
+
+class ProcessingStatus(Enum):
+    OK = 'OK'
+    FETCH_ERROR = 'FETCH_ERROR'
+    PARSING_ERROR = 'PARSING_ERROR'
+    TIMEOUT = "TIMEOUT"
 
 
 async def parse_charged_dicts(dicts_dir: str) -> list[str]:
@@ -29,13 +43,44 @@ async def parse_charged_dicts(dicts_dir: str) -> list[str]:
             return total_words
 
 
-async def process_article(session, morph, charged_words, url):
-    html = await fetch(session, url)
-    splitted_text = split_by_words(morph, sanitize(html))
-    score = calculate_jaundice_rate(splitted_text, charged_words)
-    print('URL:', url)
-    print('Рейтинг:', score)
-    print('Слов в статье:', len(splitted_text))
+async def process_article(session: aiohttp.ClientSession,
+                          morph: pymorphy2.MorphAnalyzer,
+                          charged_words: list[str],
+                          url: str,
+                          results: list[dict]):
+    try:
+        async with timeout(TIMEOUT):
+            html = await fetch(session, url)
+
+        splitted_text = split_by_words(morph, sanitize(html))
+        score = calculate_jaundice_rate(splitted_text, charged_words)
+        results.append({
+            'URL': url,
+            'Рейтинг': score,
+            'Статус': ProcessingStatus.OK.value,
+            'Слов в статье': len(splitted_text)
+        })
+    except aiohttp.ClientResponseError:
+        results.append({
+            'URL': url,
+            'Рейтинг': None,
+            'Статус': ProcessingStatus.FETCH_ERROR.value,
+            'Слов в статье': None
+        })
+    except ArticleNotFound:
+        results.append({
+            'URL': url,
+            'Рейтинг': None,
+            'Статус': ProcessingStatus.PARSING_ERROR.value,
+            'Слов в статье': None
+        })
+    except asyncio.TimeoutError:
+        results.append({
+            'URL': url,
+            'Рейтинг': None,
+            'Статус': ProcessingStatus.TIMEOUT.value,
+            'Слов в статье': None
+        })
 
 
 async def fetch(session, url):
@@ -45,11 +90,13 @@ async def fetch(session, url):
 
 
 async def main():
+    articles_scoring = []
     charged_words = await parse_charged_dicts('charged_dict')
     async with aiohttp.ClientSession() as session:
         async with anyio.create_task_group() as tg:
             for url in TEST_ARTICLES:
-                tg.start_soon(process_article, session, morph, charged_words, url)
+                tg.start_soon(process_article, session, morph, charged_words, url, articles_scoring)
+    print(articles_scoring)
 
 
 if __name__ == '__main__':
