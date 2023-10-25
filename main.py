2067ea15ffbd4815ff131ebfab8d8604c80d39a8
Aleksey Koshkin <ekbdizzy@yandex.ru>
2023-10-25 09:10:32 +0700

    Add bulk fetch of articles.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import aiohttp
 import asyncio
-import pymorphy2
 import os
+
+import aiofiles
+import aiohttp
+import anyio
+import pymorphy2
+
 from adapters.inosmi_ru import sanitize
 from text_tools import split_by_words, calculate_jaundice_rate
-import aiofiles
+
+TEST_ARTICLES = [
+    "https://inosmi.ru/20231021/neandertaltsy-266231832.html",
+    "https://inosmi.ru/20231014/solntse-266120679.html",
+    "https://inosmi.ru/20231009/obezyana-266007731.html",
+    "https://inosmi.ru/20231009/lunatizm-265977298.html",
+    "https://inosmi.ru/20231024/seks-266274274.html",
+]
 
 
 async def parse_charged_dicts(dicts_dir: str) -> list[str]:
@@ -18,6 +29,15 @@ async def parse_charged_dicts(dicts_dir: str) -> list[str]:
             return total_words
 
 
+async def process_article(session, morph, charged_words, url):
+    html = await fetch(session, url)
+    splitted_text = split_by_words(morph, sanitize(html))
+    score = calculate_jaundice_rate(splitted_text, charged_words)
+    print('URL:', url)
+    print('Рейтинг:', score)
+    print('Слов в статье:', len(splitted_text))
+
+
 async def fetch(session, url):
     async with session.get(url) as response:
         response.raise_for_status()
@@ -25,12 +45,11 @@ async def fetch(session, url):
 
 
 async def main():
+    charged_words = await parse_charged_dicts('charged_dict')
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'https://inosmi.ru/economic/20190629/245384784.html')
-        splitted_text = split_by_words(morph, sanitize(html))
-        words = await parse_charged_dicts('charged_dict')
-        rate = calculate_jaundice_rate(splitted_text, words)
-        print(rate, len(splitted_text))
+        async with anyio.create_task_group() as tg:
+            for url in TEST_ARTICLES:
+                tg.start_soon(process_article, session, morph, charged_words, url)
 
 
 if __name__ == '__main__':
